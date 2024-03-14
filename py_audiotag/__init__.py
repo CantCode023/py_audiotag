f93ced58960cb5d424662438c52b2da8ed5ecece
cantcode023 <cantcode023@gmail.com>
2024-03-14 20:50:11 +0800

    Delete py_audiotag directory

--- py_audiotag/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = '1.0.0'
-
-from py_audiotag.audiotag import Track
-from py_audiotag.audiotag import AudioTag
-
-__all__ = ["Track", "AudioTag"]
--- py_audiotag/example/main.py
+++
@@ -1,5 +0,0 @@
-from py_audiotag import AudioTag
-
-audiotag = AudioTag("YOUR_SESSION_ID_HERE")
-tracks = audiotag.recognize("./song1.mp3")
-print(tracks[0])
